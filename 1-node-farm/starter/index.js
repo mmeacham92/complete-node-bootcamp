299ae6a6874b61a026803c3d1464c4c96401c53f
mmeacham92 <meacham.michael@rocketmail.com>
2023-02-01 12:12:25 -0600

    created module for replaceTemplate function

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // Blocking, synchronous method
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -78,20 +79,3 @@ const server = http.createServer((request, response) => {
 server.listen(8000, "127.0.0.1", () => {
     console.log("Listening to requests on port 8000");
 });
-
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    }
-
-    return output;
-}
